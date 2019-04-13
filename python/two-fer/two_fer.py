0790fedb48791ffd3d75caba46d03a1cb141b509
Haithem Khelif <haithem.khelif@outlook.com>
2019-04-13 15:58:52 -0600

    Updated two-fer with a better default value

--- python/two-fer/two_fer.py
+++ python/two-fer/two_fer.py
@@ -1,5 +1,5 @@
-def two_fer(name=""):
-    if name=="":
+def two_fer(name=None):
+    if name== None:
         name="One for you, one for me."
     else:
         name="One for "+name+", one for me."
