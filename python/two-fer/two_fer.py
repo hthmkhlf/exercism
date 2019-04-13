6a512e66e88bbd0a086b6e098be63d1a9e58af61
Haithem Khelif <haithem.khelif@outlook.com>
2019-04-13 16:42:29 -0600

    made the logic simpler

--- python/two-fer/two_fer.py
+++ python/two-fer/two_fer.py
@@ -1,7 +1,3 @@
-def two_fer(name=None):
-    if name== None:
-        name="One for you, one for me."
-    else:
-        name="One for "+name+", one for me."
+def two_fer(name="you"):
 
-    return name
+    return "One for "+name+", one for me."
